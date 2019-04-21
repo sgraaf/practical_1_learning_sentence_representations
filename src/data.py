ba5086d715c4c7abe87f6ba9d3e21849783df961
sgraaf <sgraaf@users.noreply.github.com>
2019-04-21 13:16:08 +0200

    Implemented torchtext over torch-nlp

--- src/data.py
+++ src/data.py
@@ -3,21 +3,23 @@ from pathlib import Path
 import numpy as np
 import torch
 from nltk.tokenize import word_tokenize
-from torchnlp.datasets import snli_dataset
-from torchnlp.word_to_vector import GloVe
+from torchtext.data import Field
+from torchtext.datasets import SNLI
 
 data_dir = Path.cwd().parent / 'data'
 
 
-def snli(percentage=None):
+def get_SNLI(text_field, label_field, percentage=None):
     """
-    Returns the SNLI dataset, splits included
+    Returns the SNLI dataset in splits
 
+    :param torchtext.data.Field text_field: the field that will be used for premise and hypothesis data
+    :param torchtext.data.Field label_field: the field that will be used for label data
     :param float percentage: the percentage of the data to use
     :returns: the SNLI dataset in splits
     :rtype: tuple
     """
-    train, dev, test = snli_dataset(data_dir, train=True, dev=True, test=True)
+    train, dev, test = SNLI.splits(text_field, label_field)
 
     if percentage:
         train = train[:np.int(np.ceil(len(train) * percentage))]
@@ -27,31 +29,54 @@ def snli(percentage=None):
     return train, dev, test
 
 
-def tokenize(sentence):
+def get_GloVe():
     """
-    Returns the list of tokens present in the sentence
+    Returns the GloVe vectors (pre-filtered on the words present in the SNLI dataset and SentEval tasks)
 
-    :param string sentence: the sentence
-    :returns: the tokens of the sentence
-    :rtype: list
-    """
-    return [token.lower() for token in word_tokenize(sentence)]
-
-
-def glove():
-    """
-    Returns the glove vectors (pre-filtered on the SNLI dataset and SentEval tasks)
-
-    :returns: the glove vectors
+    :returns: the GloVe vectors
     :rtype: dict{str: torch.tensor}
     """
-    glove_vectors = {}
+    GloVe_vectors = {}
 
     with open(data_dir / 'small_glove_torchnlp.txt', 'r') as f:
         lines = [line.strip() for line in f.readlines()]
         for line in lines:
-            key = line.split(' ')[0]
-            value = torch.tensor(np.array(line.split(' ')[1:], dtype=float))
-            glove_vectors[key] = value
+            word, vec = line.split(' ', 1)
+            GloVe_vectors[word] = torch.tensor(np.array(vec, dtype=float))
 
-    return glove_vectors
+    return GloVe_vectors
+
+
+def load_data():
+    """
+    Load all relevant data (GloVe vectors & SNLI dataset) for our experiments
+
+    :returns: the data (train, dev, test, text_field and label_field)
+    :rtype: tuple(Dataset, Dataset, Dataset, Field, Field)
+    """
+    # get the GloVe vectors
+    GloVe_vectors = get_GloVe()
+
+    # set the dataset fields
+    text_field = Field(sequential=True,
+                       use_vocab=True,
+                       lower=True,
+                       tokenize=word_tokenize,
+                       include_lengths=True)
+
+    label_field = Field(sequential=False,
+                        use_vocab=True,
+                        pad_token=None,
+                        unk_token=None,
+                        is_target=True)
+
+    # get the SNLI dataset in splits
+    train, dev, test = get_SNLI(text_field, label_field)
+
+    # build the text_field vocabulary from all data splits
+    text_field.build_vocab(train, dev, test, vectors=GloVe_vectors)
+
+    # build the label_field vocabulary from the train split
+    label_field.build_vocab(train)
+
+    return train, dev, test, text_field, label_field
