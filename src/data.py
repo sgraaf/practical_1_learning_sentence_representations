935547ba8160f47c1a3c5a669eece8be97e11295
sgraaf <sgraaf@users.noreply.github.com>
2019-04-22 19:23:02 +0200

    Update data.py

--- src/data.py
+++ src/data.py
@@ -1,10 +1,12 @@
 from pathlib import Path
+from collections import Counter
 
 import numpy as np
 import torch
 from nltk.tokenize import word_tokenize
 from torchtext.data import Field
 from torchtext.datasets import SNLI
+from torchtext.vocab import GloVe
 
 data_dir = Path.cwd().parent / 'data'
 
@@ -56,7 +58,7 @@ def load_data(percentage=None):
     :rtype: tuple(Dataset, Dataset, Dataset, Field, Field)
     """
     # get the GloVe vectors
-    GloVe_vectors = get_GloVe()
+    GloVe_vectors = GloVe()
 
     # set the dataset fields
     text_field = Field(sequential=True,
@@ -72,7 +74,7 @@ def load_data(percentage=None):
                         is_target=True)
 
     # get the SNLI dataset in splits
-    train, dev, test = get_SNLI(text_field, label_field)
+    train, dev, test = get_SNLI(text_field, label_field, percentage)
 
     # build the text_field vocabulary from all data splits
     text_field.build_vocab(train, dev, test, vectors=GloVe_vectors)
@@ -81,3 +83,60 @@ def load_data(percentage=None):
     label_field.build_vocab(train)
 
     return train, dev, test, text_field, label_field
+
+
+def create_dictionary(sentences, threshold=0):
+    """
+    Creates a list and a dictionary to go from id to word and vice versa
+    
+    :param list sentences: the sentences
+    :param int threshold: a threshold for keeping words
+    """
+    # get a count of all words
+    words = Counter()
+    for s in sentences:
+        for word in s:
+            words[word] += 1
+
+    # throw out words that have a count below the threshold
+    if threshold > 0:
+        new_words = dict()
+        for word in words:
+            if words[word] >= threshold:
+                new_words[word] = words[word]
+        words = new_words
+    
+    words['<s>'] = 1e9 + 4
+    words['</s>'] = 1e9 + 3
+    words['<p>'] = 1e9 + 2
+
+    # create the list and dictionary
+    sorted_words = sorted(words.items(), key=lambda x: -x[1])  # inverse sort
+    id2word = []
+    word2id = {}
+    for i, (w, _) in enumerate(sorted_words):
+        id2word.append(w)
+        word2id[w] = i
+
+    return id2word, word2id
+
+
+def get_wordvec(path_to_vec, word2id):
+    """
+    Returns the word vectors (filtered on the words present in the dictionary)
+
+    :param str path_to_vec: the path to the word vectors
+    :param dict word2id: the dictionary
+    :returns: the word vectors
+    :rtype: dict{str: np.array}
+    """
+    word_vec = dict()
+
+    with open(path_to_vec, 'r', encoding='utf-8') as f:
+        lines = [line.strip() for line in f.readlines()]
+        for line in lines:
+            word, vec = line.split(' ', 1)
+            if word in word2id:
+                word_vec[word] = np.fromstring(vec, sep=' ')
+
+    return word_vec
